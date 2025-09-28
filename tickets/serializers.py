84ded07c29be3e401f38fb5688ff4c2c1555ebb1
JeremyWarui <jerywarne7@gmail.com>
2025-09-28 19:18:17 +0300

    Filter options by role of user, status of ticket or author of comments

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import *
-from django.conf import settings
+from django.contrib.auth import get_user_model
+
+# User = get_user_model()
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,9 +21,38 @@ class SectionSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    username = serializers.CharField(read_only=True)
+
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'role']
+
+    def create(self, validated_data):
+        first_name = validated_data.get('first_name')
+        last_name = validated_data.get('last_name')
+        email = validated_data.get('email', '')
+        password = validated_data['password']
+        role = validated_data.get('role', 'user')
+
+        base_username = f"{first_name.lower()}.{last_name.lower()}"
+        username = base_username
+        counter = 1
+
+        # to ensure the username is unique lets add a digit
+        while CustomUser.objects.filter(username=username).exists():
+            username = f"{username}-{counter}"
+            counter += 1
+
+        user = CustomUser.objects.create_user(
+            username=username,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            role=role,
+        )
+        return user
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .models import *
+from rest_framework import viewsets, filters
 from .serializers import *
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
@@ -16,15 +16,24 @@ class FacilityViewSet(viewsets.ModelViewSet):
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all().order_by('-created_at')
     serializer_class = TicketSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'section', 'assigned_to', 'raised_by']
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['author', 'ticket']
 
 class FeedbackViewSet(viewsets.ModelViewSet):
     queryset = Feedback.objects.all().order_by('-created_at')
     serializer_class = FeedbackSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['rating', 'rated_by', 'ticket']
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by('username')
     serializer_class = UserSerializer
+    #filter_backends = [filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['role']

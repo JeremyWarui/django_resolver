926101976b43a8b0c007cd5affbaa7b13a41757d
JeremyWarui <jerywarne7@gmail.com>
2025-10-15 01:53:06 +0300

    Refactoring views to introduce Generic Views to have standard crud instead of Viewsets which are highly abstructed. This is to have a sense of control

--- tickets/views.py
+++ tickets/views.py
@@ -1,39 +1,96 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import *
 from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
-class SectionViewSet(viewsets.ModelViewSet):
+# --------------------------------
+# SECTION
+# ----------------------------------
+class SectionListCreateView(ListCreateAPIView):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
-class FacilityViewSet(viewsets.ModelViewSet):
+class SectionDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializer
+
+# --------------------------------
+# FACILITY
+# ----------------------------------
+
+class FacilityListCreateView(ListCreateAPIView):
     queryset = Facility.objects.all()
     serializer_class = FacilitySerializer
 
-class TicketViewSet(viewsets.ModelViewSet):
+class FacilityDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Facility.objects.all()
+    serializer_class = FacilitySerializer
+
+# --------------------------------
+# TICKETS
+# ----------------------------------
+
+class TicketListCreateView(ListCreateAPIView):
     queryset = Ticket.objects.all().order_by('-created_at')
     serializer_class = TicketSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['status', 'section', 'assigned_to', 'raised_by']
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
+    def perform_create(self, serializer):
+        """Delegate ticket creation to service layer"""
+       # services.create_ticket(serializer, self.request.user)
+
+class TicketDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    def perform_update(self, serializer):
+        """ delegate ticket update ( assign, update status, etc) """
+        # services.update_ticket(serializer, self.request.user)
+
+# --------------------------------
+# COMMENTS
+# ----------------------------------
+
+class CommentListCreateView(ListCreateAPIView):
+    queryset = Comment.objects.all().order_by('created_at')
     serializer_class = CommentSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['author', 'ticket']
 
-class FeedbackViewSet(viewsets.ModelViewSet):
+    def perform_create(self, serializer):
+        # ticket_id = self.request.data.get('ticket_id')
+        # services.create_comment(serializer, self.request.user, ticket_id)
+
+
+# --------------------------------
+# FEEDBACK
+# ----------------------------------
+
+class FeedbackListCreateView(ListCreateAPIView):
     queryset = Feedback.objects.all().order_by('-created_at')
     serializer_class = FeedbackSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['rating', 'rated_by', 'ticket']
 
-class UserViewSet(viewsets.ModelViewSet):
+    def perform_create(self, serializer):
+        # ticket_id = self.request.data.get('ticket_id')
+        # services.create_feedback(serializer, self.request.user, ticket_id)
+
+
+# --------------------------------
+# FEEDBACK
+# ----------------------------------
+
+class UserListCreateView(ListCreateAPIView):
     queryset = CustomUser.objects.all().order_by('username')
     serializer_class = UserSerializer
-    #filter_backends = [filters.SearchFilter]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['role']
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer

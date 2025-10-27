d7363d6b7771f814427db64248733cfd6436be6d
JeremyWarui <jerywarne7@gmail.com>
2025-10-27 23:06:35 +0300

    Prepare settings file for production by adding Postgresql database

--- resolver/settings.py
+++ resolver/settings.py
@@ -10,24 +10,35 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
+
 from pathlib import Path
 
 from tickets.apps import TicketsConfig
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load .env file
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-evo47-7z$&aazzyp7_cqf85%!q2)wsrw*lb)+3ab9+$0%h6(=5'
+# SECRET_KEY = 'django-insecure-evo47-7z$&aazzyp7_cqf85%!q2)wsrw*lb)+3ab9+$0%h6(=5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', 'localhost'
+]
 
 
 # Application definition
@@ -43,6 +54,7 @@ INSTALLED_APPS = [
     'tickets.apps.TicketsConfig',
     # add django rest framework
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
 ]
 
@@ -92,8 +104,14 @@ WSGI_APPLICATION = 'resolver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 

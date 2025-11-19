23b6a5c6fa56977e612019a7396036a6ebaedc70
JeremyWarui <jerywarne7@gmail.com>
2025-11-20 00:28:52 +0300

    Update cors origin to include new frontend domain

--- resolver/settings.py
+++ resolver/settings.py
@@ -182,8 +182,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = os.getenv(
     'ALLOWED_ORIGINS',
-    'http://localhost:5173,',
-    'http://127.0.0.1:5173'
+    'http://localhost:5173, http://127.0.0.1:5173',
 ).split(',')
 
 # Allow credentials (cookies, authorization headers, etc.)

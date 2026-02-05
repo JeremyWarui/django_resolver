48d92825443caacc7d66119d73c2e5db90c9c28e
JeremyWarui <jerywarne7@gmail.com>
2026-02-05 03:28:26 +0300

    Fix Django Unfold static files for Render deployment

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Collect static files (important for Django Unfold)
+python manage.py collectstatic --no-input --clear
+
+# Run migrations
 python manage.py migrate
--- resolver/settings.py
+++ resolver/settings.py
@@ -172,8 +172,15 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# WhiteNoise configuration
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# WhiteNoise configuration for Django Unfold compatibility
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
